e688f535a28c8af2d7ec988515d9c2c43083fb2a
Arpita Nag <anag@stuy.edu>
2016-12-30 21:14:52 -0500

    Added panel images, abstract Button class, class for the blue button, and main 'driver class' for setting up UI

---
+++ ArpitaN/Button1.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.swing.*;
+
+public  class Button1 extends Button{
+    public SetBackground panel;
+    
+    public Button1(BufferedImage img, SetBackground bg){
+        super(141, 340, img);
+        panel = bg;
+    }
+    
+    public void actionCommand(){
+            ImageIcon panel_bg = new ImageIcon("mode" + (btn_choice % 8) + ".png");
+            Image newimg = panel_bg.getImage().getScaledInstance(246, 240, Image.SCALE_SMOOTH);
+             panel.default_choice.setIcon(new ImageIcon (newimg));
+            BufferedImage buffered = new BufferedImage(newimg.getWidth(null), newimg.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+            buffered.getGraphics().drawImage(newimg, 0, 0, null);
+            ImageIcon darker = new ImageIcon(buffered);
+            panel.default_choice.setIcon(darker);
+            if(btn_choice % 8 == 6) panel.dark = !(panel.dark);
+            if(panel.dark) SetBackground.makeDarker(buffered);
+    }
+
+
+
+
+}
